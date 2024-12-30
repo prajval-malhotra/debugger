fe693d4f4a1441680b0cacc30d6c4dcf07fb999e
prajval-malhotra <p6malhot@uwaterloo.ca>
2024-12-30 14:53:21 -0500

    attach, pause, and continue proccesses

--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -1,5 +1,145 @@
-#include <libsdb/libsdb.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-int main() {
-    sdb::say_hello();
+namespace {
+    std::vector<std::string> split(std::string_view str, char delimiter) {
+        std::vector<std::string> out{};
+        std::stringstream ss {std::string{str}};
+        std::string item;
+
+        while(std::getline(ss, item, delimiter)) {
+            out.push_back(item);
+        }
+
+        return out;
+    }
+
+    bool is_prefix(std::string_view str, std::string_view of) {
+        if(str.size() > of.size()) {
+            return false;
+        }
+
+        // check if "str" is a prefix of "of"
+        return std::equal(str.begin(), str.end(), of.begin());
+    }
+
+    void resume(pid_t pid) {
+        // continue executing the process
+        if(ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0) {
+            std::cerr << "Couldnt continue\n";
+            std::exit(-1);
+        }
+    }
+
+    void wait_on_signal(pid_t pid) {
+        int wait_status;
+        int options = 0;
+        if(waitpid(pid, &wait_status, options) < 0) {
+            std::perror("waitpid failed");
+            std::exit(-1);
+        }
+    }
+
+    pid_t attach(int argc, const char** argv) {
+        pid_t pid = 0;
+        if(argc == 3 && argv[1] == std::string_view("-p")) { // passing the PID
+            pid = std::atoi(argv[2]);
+            
+            if(pid <= 0) {
+                std::cerr << "Invalid pid\n";
+                return -1;
+            }
+
+            if(ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0) {
+                // attach to process and pause its execution (SIGSTOPped)
+                std::perror("Could not attach");
+                return -1;
+            }
+
+        } else { // passing the program name
+            const char *program_path = argv[1];
+            
+            if((pid = fork()) < 0) { // create new proc
+                std::perror("fork failed");
+                return -1;
+            }
+
+            if(pid == 0) { // in child process
+                if(ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) { // setup new proc for tracing
+                    std::perror("Tracing failed");
+                    return -1;
+                }
+
+                if(execlp(program_path, program_path, nullptr) < 0) { // start new proc
+                    std::perror("Exec failed");
+                    return -1;
+                }
+            }
+        }
+
+        return pid;
+    }
+
+    void handle_command(pid_t pid, std::string_view line) {
+        std::vector<std::string> args = split(line, ' ');
+        std::string command = args[0];
+
+        if(is_prefix(command, "continue")) {
+            resume(pid);
+            wait_on_signal(pid);
+        } else {
+            std::cerr << "Unknown command\n";
+        }
+    }
+
+};
+
+
+int main(int argc, const char **argv) {
+    if(argc == 1) {
+        std::cerr << "No arguments given\n";
+        return -1;
+    }
+
+    pid_t pid = attach(argc, argv);
+
+    int wait_status;
+    int options = 0;
+    if(waitpid(pid, &wait_status, options) < 0) {
+        std::perror("waitpid failed");
+    }
+
+    char *line = nullptr;
+    while((line = readline("sdb> ")) != nullptr) {
+        std::string line_str;
+
+        if(line == std::string_view("")) {
+            free(line);
+            if(history_length > 0) {
+                line_str = history_list()[history_length - 1]->line;
+            }
+        } else {
+            line_str = line;
+            add_history(line);
+            free(line);
+        }
+
+        if(!line_str.empty()) {
+            handle_command(pid, line_str);
+            continue;
+        }
+
+        handle_command(pid, line);
+        add_history(line);
+        free(line);
+    }
 }
